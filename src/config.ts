7d31d60777ff801f47a59ef6d0b25153579d027b
b-caby <caby.berenger@gmail.com>
2019-07-23 23:53:46 +0200

    fix: solve port issues with Heroku

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ if (fs.existsSync(".env")) {
 }
 
 const debugLevel = process.env["NODE_ENV"] === "production" ? "info" : "debug";
-const port = process.env["NODE_ENV"] === "production" ? 80 : 789;
+const port = process.env.PORT || 789;
 const host = process.env["HOST"];
 const user = process.env["USER"];
 const password = process.env["PASSWORD"];
