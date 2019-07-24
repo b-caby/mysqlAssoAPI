b4c8048c9ad0fa1c4e67ef0f5b007ca5e899bb59
b-caby <caby.berenger@gmail.com>
2019-07-24 13:49:18 +0200

    feat: added CORS features

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express      from "express";
 import bodyParser   from "body-parser";
 import { port }     from "./config";
 import routes       from "./api/routes";
+import helmet       from "helmet";
+import cors         from "cors";
 
 // Create Express server
 const app = express();
@@ -10,6 +12,16 @@ const app = express();
 app.set("port", port);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
+app.disable("x-powered-by");
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, Authorization, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 // Primary app routes
 app.use("/api", routes);
